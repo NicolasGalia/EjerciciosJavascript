9cc42cc551721ebf57cee4602a312f8e64cf2c7e
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-07-05 23:34:16 -0300

    realizamos los ejercicios 8 9 10 y 11

---
+++ js/9-ejercicio.js
@@ -0,0 +1,9 @@
+let text = prompt("Escribe una frase");
+let nText = text.length;
+let i;
+for (i = 0; i < nText; i++) {
+if (string.length(i,1) === "a" || string.length(i,1) === "e" || string.length(i,1) === "i" || string.length(i,1) === "o" || string.length(i,1) === "u");
+
+document.write("las vocales son" + string.length(i,1);
+
+}
